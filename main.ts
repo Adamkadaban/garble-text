b9bced802aebb274f1af0666f54310374b7af090
Adamkadaban <adamkadaban@gmail.com>
2024-09-02 15:26:39 -0400

    add option to not garble text on hover

--- main.ts
+++ main.ts
@@ -1,13 +1,19 @@
 import "./css/image-blur.css";
 import "./css/ungarble-on-hover.css";
 import { App, Plugin } from 'obsidian';
+import { DEFAULT_SETTINGS, GarbleTextSettings, GarbleTextSettingsTab } from './settings';
 
 export default class GarbleText extends Plugin {
 	private isGarbled: boolean = false;
 
+
+	settings: GarbleTextSettings;
+
 	async onload() {
 		console.log('loading Garble Text plugin');
 
+		await this.loadSettings();
+		this.addSettingTab(new GarbleTextSettingsTab(this.app, this));
 		this.addCommand({
 			id: "toggle-garble-text",
 			name: "Toggle Garble Text",
@@ -19,12 +25,30 @@ export default class GarbleText extends Plugin {
 			}
 		});
 
+		if (!this.settings.noGarbleOnHover) {
+			const link = document.createElement("link");
+			link.rel = "stylesheet";
+			link.type = "text/css";
+			link.href = "./css/ungarble-on-hover.css";
+			document.head.appendChild(link);
+		}
+
 		//TODO: garble with regex
 	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+
 
 	onunload() {
 		this.isGarbled = false;
 		this.ungarble();
+		const link = document.getElementById(this.ungarbleCssId);
+		if (link) {
+			link.remove();
+		}
 		console.log('Garble Text unloaded');
 	}
 
---
+++ settings.ts
@@ -0,0 +1,35 @@
+export interface GarbleTextSettings {
+	noGarbleOnHover: boolean;
+}
+
+export const DEFAULT_SETTINGS: GarbleTextSettings = {
+	noGarbleOnHover = false,
+}
+
+export class GarbleTextSettingsTab extends Plugins SettingTab {
+	plugin: GarbleText;
+
+	constructor(app: App, plugin: GarbleText) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		let { containerEl } = this;
+
+		constainerEl.empty();
+
+		containerEl.createEl('h2', { text: 'Garble Text Plugin' });
+
+		new Setting(containerEl)
+			.setName('Do not garble text on hover')
+			.setDesc('If this is turned on, text will no longer be revealed when hovered over with a mouse.')
+			.addToggle(cb => {
+				cb.setValue(this.plugin.settings.noGarbleOnHover)
+					.onChange(async value => {
+						this.plugin.settings.noGarbleOnHover = value;
+						await this.plugin.saveSettings();
+					})
+			});
+	}
+}
